9775875e933ddc333ea866e39d2fb545e89068e9
alanbalugu <55846088+alanbalugu@users.noreply.github.com>
2022-10-14 13:21:21 -0400

    Add files via upload

---
+++ evals_gen.py
@@ -0,0 +1,137 @@
+import time
+
+from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver import ActionChains
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support import expected_conditions as EC
+
+from datetime import datetime
+
+import json
+import re
+
+def main():
+
+    driver = webdriver.Chrome('---location of chromedriver---').  #***
+    driver.get("https://www.new-innov.com/login/sso/whc")
+    time.sleep(2)
+
+    uname = "abc123".  #***
+    pword = "net-id-password"   #***
+
+    unameArea = driver.find_element("id", "username")
+    unameArea.send_keys(uname)
+
+    pwdArea = driver.find_element("id", "password")
+    pwdArea.send_keys(pword)
+    time.sleep(1)
+
+    action = ActionChains(driver)
+    login = driver.find_element("name", "_eventId_proceed")
+    action.move_to_element(login).perform()
+    login.click()
+    time.sleep(2)
+
+    action = ActionChains(driver)
+
+    driver.switch_to.frame(driver.find_element("id","duo_iframe"))
+    enterPass = driver.find_element("id","passcode")
+    action = ActionChains(driver)
+    action.move_to_element(enterPass).perform()
+    enterPass.click()
+    time.sleep(2)
+
+    typePass = driver.find_element("name","passcode")
+    typePass.send_keys("#########") #***
+
+    loginDuo = driver.find_element("id","passcode")
+    action.move_to_element(loginDuo).perform()
+    loginDuo.click()
+    time.sleep(10)
+
+    driver.get("https://www.new-innov.com/EvaluationForms/EvaluationFormsHost.aspx?Control=CompleteEvals")
+    time.sleep(7)
+
+    evals = driver.find_elements(By.CLASS_NAME, "sessionColumn")
+
+    action = ActionChains(driver)
+
+    for i in range(0, len(evals)):
+
+        item = driver.find_elements(By.CLASS_NAME, "sessionColumn")[0]
+        action.move_to_element(item).perform()
+        item.click()
+        time.sleep(7)
+
+        #agrees = driver.find_elements(By.XPATH, "//input[@data-id = '61795'")
+        agrees = driver.find_elements(By.CLASS_NAME, "ni-radio")
+
+        counter = 0
+
+        name = agrees[0].get_attribute("name")
+
+        time.sleep(2)
+        for question in agrees:
+
+            if(counter == 0):
+                name = question.get_attribute("name")
+                print(name)
+
+            counter += 1
+            click = False
+
+            if(question.get_attribute("name") == name):
+                if((counter%5) == 4):
+                    action = ActionChains(driver)
+                    driver.execute_script("arguments[0].scrollIntoView();", question)
+                    time.sleep(1)
+                    action.move_to_element(question).perform()
+                    try:
+                        question.click()
+                    except:
+                        time.sleep(0.1)
+                    time.sleep(0.5)
+                if((counter%5)==0):
+                    counter = 0
+            else:
+                counter = 1
+
+            time.sleep(0.2)
+
+        time.sleep(1)
+
+        html = driver.find_element(By.TAG_NAME, "html")
+        html.send_keys(Keys.END)
+
+        try:
+            submit = driver.find_element(By.XPATH,'/html/body/form/div[4]/div[2]/div/div[1]/div/div/div[3]/div/div[2]/div[1]/div/button')
+            driver.execute_script("arguments[0].scrollIntoView();", submit)
+            time.sleep(1)
+            submit.click()
+        except Exception as e:
+            print("err", e)
+
+        try:
+            #submit = driver.find_element(By.CLASS_NAME,"btn ni-button btn-primary")
+            submit = driver.find_element("xpath", "//div[@class = 'right']//button").click()
+            driver.execute_script("arguments[0].scrollIntoView();", submit)
+            time.sleep(1)
+            submit.click()
+        except Exception as e:
+            print("err", e)
+
+        # action = ActionChains(driver)
+        # action.move_to_element(submit).perform()
+        time.sleep(20)
+        # submit.click()
+
+    driver.quit()
+
+if __name__ == "__main__":
+    main()
---
+++ evals_readme.txt
@@ -0,0 +1,11 @@
+INSTRUCTIONS
+
+1. Install Python
+2. Install Google Chrome if you don't already have it
+2. Check version of chrome and download the correct “chromedriver” version
+3. Install the “selenium” python module using either pip or conda based on your python install
+4. Edit the script with the following (search for “ *** ” to find these lines):
+	1. Location of chromedriver on your computer
+	2. Your netID and password
+	3. The last 9 digits of the long id number on the back of your gocard
+5. Run the python script and just let it do its thing for a while.
